7b90fb99006c30cc0123cddff0ad0b45366f27d6
Vladimir Zakharov <vladimzakh4rov@yandex.ru>
2022-05-07 17:30:57 +0300

    реализована регистрация и авторизация пользователей

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -6,7 +6,7 @@ function InfoTooltip(props) {
         <div className={`popup popup_${props.name} ${props.isOpen ? 'popup_opened' : ''}`}>
             <div className={`popup__container popup__container_${props.name}`}>
                 <button className={`popup__close-btn popup__close-btn_${props.name}`} onClick={props.onClose}></button>
-                <img src={props.image} className={`popup__img popup__img_${props.name}`} />
+                <img src={props.image} alt="Картинка успешной(или не очень) регистрации" className={`popup__img popup__img_${props.name}`} />
                 <h2 className={`popup__title popup__title_${props.name}`}>
                     {props.title}
                 </h2>
